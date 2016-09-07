1d866be381c6e4de39f0e82b61fc8388d20262d8
armstrongmsg <armstrongmsg@gmail.com>
2016-09-07 13:31:52 -0300

    Added .png to gitignore and plot output to the analysis script

--- analysis.R
+++ analysis.R
@@ -5,3 +5,5 @@ results <- read.csv("results.csv")
 results_summary <- results %>% group_by(CAP) %>% summarise(mean_time = mean(time))
 ggplot(results_summary, aes(CAP, mean_time)) + geom_line()
 
+ggsave("cpucap.png")
+
