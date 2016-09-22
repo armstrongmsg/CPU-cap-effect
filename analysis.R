9aae65d3b6e24a5ab26066ed736d3ef355c0951d
armstrongmsg <armstrongmsg@gmail.com>
2016-09-22 15:11:28 -0300

    Added normalized standard deviation and performance metric plots

--- analysis.R
+++ analysis.R
@@ -2,8 +2,20 @@ require(ggplot2)
 require(dplyr)
 
 results <- read.csv("results.csv")
+results <- results %>% group_by(CAP) %>% arrange(-time) %>% mutate(normalized_time = (time - min(time))/ (max(time) - min(time)))
+
 results_summary <- results %>% group_by(CAP) %>% summarise(mean_time = mean(time))
+results_sd <- results %>% group_by(CAP) %>% summarise(sd = sd(time))
+results_sd$mean <- results_summary$mean_time
+results_sd_normalized <- results %>% group_by(CAP) %>% summarise(sd = sd(normalized_time))
+
 ggplot(results_summary, aes(CAP, mean_time)) + geom_line()
+ggplot(results_summary %>% filter(CAP >= 50), aes(CAP, mean_time)) + geom_line()
+
+ggplot(results_sd_normalized, aes(CAP, sd)) + geom_line()
+
+results_summary <- results_summary %>% mutate(performance_loss = c(-diff(results_summary$mean_time)/results_summary$mean_time[2:length(results_summary$mean_time)],0))
+ggplot(results_summary, aes(CAP, performance_loss)) + geom_line()
 
 ggsave("cpucap.png")
 
